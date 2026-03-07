cf01613d3c8970abd44bfea98328569162bdf68f
MickaelDutfoy <m.dutfoy@gmail.com>
2026-03-07 10:42:16 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,185 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Element {
+    Fire,
+    Water,
+    Air,
+    Earth,
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+struct Pool {
+    fire: u8,
+    water: u8,
+    air: u8,
+    earth: u8,
+}
+
+impl Pool {
+    fn up(&mut self, element: Element) {
+        match element {
+            Element::Fire => self.fire += 1,
+            Element::Water => self.water += 1,
+            Element::Air => self.air += 1,
+            Element::Earth => self.earth += 1,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+struct PlayerState {
+    max_pool: Pool,
+    current_pool: Pool,
+}
+
+impl PlayerState {
+    fn start_turn(&mut self, up: Element) {
+        self.max_pool.up(up);
+        self.current_pool = self.max_pool;
+    }
+
+    fn try_pay(&mut self, cost: Pool) -> bool {
+        if cost.fire > self.current_pool.fire
+            || cost.air > self.current_pool.air
+            || cost.earth > self.current_pool.earth
+            || cost.water > self.current_pool.water
+        {
+            return false;
+        }
+
+        self.current_pool.fire -= cost.fire;
+        self.current_pool.air -= cost.air;
+        self.current_pool.earth -= cost.earth;
+        self.current_pool.water -= cost.water;
+
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn turn_1_up_fire_refreshes_current() {
+        let mut player = PlayerState::default();
+
+        player.start_turn(Element::Fire);
+
+        assert_eq!(
+            player.max_pool,
+            Pool {
+                fire: 1,
+                ..Pool::default()
+            }
+        );
+        assert_eq!(
+            player.current_pool,
+            Pool {
+                fire: 1,
+                ..Pool::default()
+            }
+        );
+    }
+
+    #[test]
+    fn turn_2_up_water_accumulates_in_max_and_current() {
+        let mut player = PlayerState::default();
+
+        player.start_turn(Element::Fire);
+        player.start_turn(Element::Water);
+
+        assert_eq!(
+            player.max_pool,
+            Pool {
+                fire: 1,
+                water: 1,
+                ..Pool::default()
+            }
+        );
+        assert_eq!(
+            player.current_pool,
+            Pool {
+                fire: 1,
+                water: 1,
+                ..Pool::default()
+            }
+        );
+    }
+
+    #[test]
+    fn refresh_overwrites_spent_current_pool() {
+        let mut player = PlayerState::default();
+
+        player.start_turn(Element::Fire);
+        player.current_pool.fire = 0; // simulate consuming fire
+        player.start_turn(Element::Air);
+
+        assert_eq!(
+            player.max_pool,
+            Pool {
+                fire: 1,
+                air: 1,
+                ..Pool::default()
+            }
+        );
+        assert_eq!(
+            player.current_pool,
+            Pool {
+                fire: 1,
+                air: 1,
+                ..Pool::default()
+            }
+        );
+    }
+
+    #[test]
+    fn cant_pay_mana_cost() {
+        let mut player = PlayerState::default();
+
+        player.start_turn(Element::Water);
+
+        let cost = Pool {
+            water: 2,
+            ..Pool::default()
+        };
+
+        let result = player.try_pay(cost);
+
+        assert_eq!(result, false);
+
+        assert_eq!(
+            player.current_pool,
+            Pool {
+                water: 1,
+                ..Pool::default()
+            }
+        );
+    }
+
+    #[test]
+    fn successful_payment_decreases_multiple_elements() {
+        let mut player = PlayerState::default();
+
+        player.start_turn(Element::Water);
+        player.start_turn(Element::Water);
+        player.start_turn(Element::Earth);
+
+        let cost = Pool {
+            water: 1,
+            earth: 1,
+            ..Pool::default()
+        };
+
+        let result = player.try_pay(cost);
+
+        assert_eq!(result, true);
+
+        assert_eq!(
+            player.current_pool,
+            Pool {
+                water: 1,
+                ..Pool::default()
+            }
+        );
+    }
+}
